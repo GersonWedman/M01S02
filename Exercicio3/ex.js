978428e3bfa27cb2d1dfe1ae73d40ad200fd5eff
gersonwedman <gerson_wedman-filho@estudante.sesisenai.org.br>
2022-12-08 16:35:50 -0300

    correção no funcionamento do código

--- Exercicio3/ex.js
+++ Exercicio3/ex.js
@@ -1,8 +1,8 @@
 var numero = prompt('Digite o número a realizar as multilplicações:');
 
 let i = 0;
-for (i = 0; i < 10; i++) {
-  var mult = i + 1;
+for (i = 0; i < 11; i++) {
+  var mult = i;
   var display = numero * mult;
   alert(numero + ' x ' + mult + ' = ' + display);
 }
