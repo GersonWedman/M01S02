39184c7e6da1e6c31b75cd4915862e8322fcc48c
gersonwedman <gerson_wedman-filho@estudante.sesisenai.org.br>
2022-12-09 20:06:50 -0300

    correção

--- Exercicio2/ex.js
+++ Exercicio2/ex.js
@@ -3,3 +3,6 @@ var idade = prompt('Digite a idade:');
 if (idade <= 15 && idade >= 0) alert('Jovem');
 else if (idade > 15 && idade <= 64) alert('Adulto');
 else if (idade > 64) alert('Idoso');
+else {
+  alert('valor inválido');
+}
