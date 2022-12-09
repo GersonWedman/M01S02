fd930c37ba07fcc4b48d6b8de8c76e2a124ee115
gersonwedman <gerson_wedman-filho@estudante.sesisenai.org.br>
2022-12-09 15:18:23 -0300

    Exercicio 8 - trello - calculadora P.A.

---
+++ Exercicio8/ex.js
@@ -0,0 +1,15 @@
+var inicial = 0;
+var raiz = 0;
+inicial = window.prompt('Digite o valor inicial:');
+inicial = parseInt(inicial);
+raiz = window.prompt('Digite a raiz:');
+raiz = parseInt(raiz);
+
+alert(inicial);
+alert(inicial + raiz);
+resultado = inicial + raiz;
+
+for (i = 0; i < 11; i++) {
+  resultado += raiz;
+  alert(resultado);
+}
