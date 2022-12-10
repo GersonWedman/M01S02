1e06bcfdfb5809ff484edb7e5cf72a107c1bbea9
gersonwedman <gerson_wedman-filho@estudante.sesisenai.org.br>
2022-12-09 21:48:09 -0300

    Exercicio 7 - numero primo

---
+++ Exercicio7/ex.js
@@ -0,0 +1,20 @@
+// Percorra os números de o a 1000 e exiba no console os números primos e ao final a quantidade de números primos que existem nesse intervalo.
+// Realize o commit do exercício resolvido.
+
+var quantidade = 0;
+
+for (var i = 2; i <= 1000; i++) {
+  var primo = true;
+
+  for (var j = 2; j < i; j++) {
+    if (i % j === 0) {
+      primo = false;
+    }
+  }
+
+  if (primo) {
+    quantidade += 1;
+    console.log(i);
+  }
+}
+console.log('Quantidade de primos entre 0 e 1000: ' + quantidade);
